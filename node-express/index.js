a172d8914c4ebb550a24ce98641ca5959ad266be
harish250 <40708202+harish250@users.noreply.github.com>
2020-07-02 21:53:12 +0530

    completed assingment - 1 and started week2

--- node-express/index.js
+++ node-express/index.js
@@ -1,15 +1,24 @@
 const express = require('express'),
      http = require('http');
-
+const bodyparser = require('body-parser');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-
 const morgan = require("morgan");
-app.use(morgan('dev'));
 
+app.use(morgan('dev'));
+app.use(bodyparser.json());
 app.use(express.static(__dirname+'/public'));
+
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+app.use('/dishes', dishRouter);
+app.use('/leaders',leaderRouter);
+app.use('/promotions',promoRouter);
+
+
 
 app.use((req, res, next) => {
   res.statusCode = 200;
