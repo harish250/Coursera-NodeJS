6ab6539d216267c922e4c2ccaa1325c3fbe6063a
harish250 <40708202+harish250@users.noreply.github.com>
2020-07-04 11:56:01 +0530

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,36 +1,43 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
-const url = 'mongodb://localhost:27017/';
-const dbname = 'conFusion';
+const url = "mongodb://localhost:27017/";
+const dbname = "conFusion";
+const dboperations = require("./operations");
 
 MongoClient.connect(url, (err, client) => {
+  assert.equal(err, null);
 
-    assert.equal(err,null);
+  console.log("Connected correctly to server");
 
-    console.log('Connected correctly to server');
+  const db = client.db(dbname);
+  dboperations.insertDocument(db,{name: "Vadonut", description: "Test"},"dishes",result=>
+  {
+    console.log("Insert Document:\n",result.ops);
 
-    const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
+    dboperations.findDocuments(db,"dishes",(docs)=>
+    {
+      console.log("Found Documents:\n", docs);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+      dboperations.updateDocument(db,{ name: "Vadonut" },
+      { description: "Updated Test" }, "dishes",(result)=>
+      {
+        console.log("Updated Document:\n", result.result);
+        
+        dboperations.findDocuments(db,"dishes",(docs)=>
+        {
+          console.log("Found Updated Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+          db.dropCollection("dishes",(result)=>
+          {
+            console.log("Dropped Collection: ", result);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
-
-                client.close();
-            });
-        });
+            client.close();
+          });
+        })
+      });
     });
 
+  })
+  
 });
