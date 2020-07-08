57cc8755d7c00b9331b6d865c7a6b43746421b3d
harish250 <40708202+harish250@users.noreply.github.com>
2020-07-08 22:04:43 +0530

    Cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,6 +14,7 @@ const promotionRouter = require('./routes/promoRouter');
 // view engine setup
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
+const { read } = require('fs');
 const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
@@ -27,15 +28,60 @@ connect.then((gb)=>
   console.log(err);
 })
 
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('1234-456-789-1011'));
 
+function auth (req, res, next) {
+  if(!req.signedCookies.user)
+  {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+        return;
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+      res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');      
+        err.status = 401;
+        next(err);
+    }
+  }
+  else
+  {
+    if(req.signedCookies.user === 'admin')
+    {
+      next();
+    } 
+    else
+    {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+    }
+  }
+  
+  
+}
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,51 +1,108 @@
-const bodyparser = require('body-parser');
-const express = require('express');
+const bodyparser = require("body-parser");
+const express = require("express");
 
 const promoRouter = express.Router();
 
 promoRouter.use(bodyparser.json());
-promoRouter.route('/').all((req,res,next)=>
-{
-    res.statusCode=200;
-    res.setHeader('Content-Type',"text/plain");
-    next();
-}).get((req,res,next)=>
-{
-    res.end('Will send all the promotions to you!');
-}).post((req,res,next)=>
-{
-    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
-}).put((req,res,next)=>
-{
-    res.statusCode = 403;
-    res.send('PUT operation not supported on /promotions');
-}).delete((req,res,next)=>
-{
-    res.end('Deleting all promotions');
-});
+const Promotions = require("../models/promotions");
 
-
-promoRouter.route('/:promoId').all((req,res,next)=>
-{
-    res.statusCode=200;
-    res.setHeader("Content-Type","text/plain");
-    next();
-}).get((req,res,next)=>
-{
-    res.end('Will send details of the promotion: ' + req.params.promoId +' to you!');
-}).post((req,res,next)=>
-{
+promoRouter
+  .route("/")
+  .get((req, res, next) => {
+    Promotions.find({})
+      .then(
+        (promotions) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promotions);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => {
+        next(err);
+      });
+  })
+  .post((req, res, next) => {
+    Promotions.create(req.body)
+      .then(
+        (promotion) => {
+          console.log("Promotion Create ", promotion);
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promotion);
+        },
+        (err) => {
+          next(err);
+        }
+      )
+      .catch((err) => next(err));
+  })
+  .put((req, res, next) => {
     res.statusCode = 403;
-    res.end('POST operation not supported on /promotions/'+ req.params.promoId);
-}).put((req,res,next)=>
-{
-    res.write('Updating the promotion: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion: ' + req.body.name + 
-          ' with details: ' + req.body.description);
-}).delete((req,res,next)=>
-{
-    res.end('Deleting promotion: ' + req.params.promoId);
-});
+    res.send("PUT operation not supported on /promotions");
+  })
+  .delete((req, res, next) => {
+    Promotions.remove({})
+      .then(
+        (resp) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(resp);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  });
+
+promoRouter
+  .route("/:promoId")
+  .get((req, res, next) => {
+    Promotions.findById(req.params.promoId)
+      .then(
+        (promotion) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promotion);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(
+      "POST operation not supported on /promotions/" + req.params.promoId
+    );
+  })
+  .put((req, res, next) => {
+    Promotions.findByIdAndUpdate(
+      req.params.promoId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then(
+        (promotion) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promotion);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Promotions.findByIdAndRemove(req.params.promoId)
+      .then(
+        (resp) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(resp);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  });
 
 module.exports = promoRouter;
-
